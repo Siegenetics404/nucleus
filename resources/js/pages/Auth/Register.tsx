d2441c3d0b5db3c944afe0ce8bd6fdeb88c7c5b5
Prince Sanguan <132465461+PrinceSanguan@users.noreply.github.com>
2026-01-20 17:33:49 +0800

    push

--- resources/js/pages/Auth/Register.tsx
+++ resources/js/pages/Auth/Register.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Alert, AlertDescription } from '@/components/ui/alert';
 import { Head, useForm } from '@inertiajs/react';
 import { FormEvent } from 'react';
 
